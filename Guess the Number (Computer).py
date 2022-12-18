1a84385b3a8a31049eb37e5841251720815fb290
Michael Samiotis <93983609+MichaelSamiotis@users.noreply.github.com>
2022-12-18 20:46:09 +0000

    Update Guess the Number (Computer).py

--- Guess the Number (Computer).py
+++ Guess the Number (Computer).py
@@ -1,58 +1,30 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "a46ba8cf",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#import the random package so we can call the random.randint function\n",
-    "import random\n",
-    "\n",
-    "#define the upper limit for the range of the guessing numbers\n",
-    "upper_limit=100\n",
-    "\n",
-    "#invoke the random.randint(x,y) function\n",
-    "random_number = random.randint(1,upper_limit)\n",
-    "\n",
-    "guessing = 0\n",
-    "\n",
-    "#Create a loop when the user inputs a random number and return the appropriate message\n",
-    "while random_number != guessing:\n",
-    "\n",
-    "#Ask a guessing number from the user\n",
-    "    guessing = int(input(f'Please guess a number from 1 to {upper_limit}: '))\n",
-    "\n",
-    "#Provide feedback to the user\n",
-    "    if random_number > guessing:\n",
-    "        print('Please guess again. Your guess is too low.')\n",
-    "    elif random_number < guessing:\n",
-    "        print('Please guess again. Your guess is too high.')\n",
-    "        \n",
-    "print(f'Congrats, you guessed correct! The secret number is {random_number}.')"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.2"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+#import the random package so we can call the random.randint function
+import random
+
+#define the upper limit for the range of the guessing numbers
+upper_limit=100
+
+#invoke the random.randint(x,y) function
+random_number = random.randint(1,upper_limit)
+
+guessing = 0
+
+#Create a loop when the user inputs a random number and return the appropriate message
+while random_number != guessing:
+
+#Ask a guessing number from the user
+    guessing = int(input(f'Please guess a number from 1 to {upper_limit}: '))
+
+#Provide feedback to the user
+    if random_number > guessing:
+        print('Please guess again. Your guess is too low.')
+    elif random_number < guessing:
+        print('Please guess again. Your guess is too high.')
+        
+print(f'Congrats, you guessed correct! The secret number is {random_number}.')
+
+#Handling of the program exit
+import sys
+exit_program = input('Press any key to exit')
+if exit_program:
+    sys.exit()
